02af9e63950c48d14930404ab83ffccc1cca1762
Hyok An <anhyok@gmail.com>
2024-06-04 04:08:52 +0900

    .

--- src/main/java/com/example/firstproject/api/CommentApiController.java
+++ src/main/java/com/example/firstproject/api/CommentApiController.java
@@ -1,11 +1,38 @@
 package com.example.firstproject.api;
 
+import com.example.firstproject.dto.CommentDto;
 import com.example.firstproject.service.CommentService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class CommentApiController {
     @Autowired
     private CommentService commentService;
+
+    // 1. Retrieve comments
+    @GetMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<List<CommentDto>> comments(@PathVariable Long articleId) {
+        // Request to service
+        List<CommentDto> dtos = commentService.comments(articleId);
+        // Response the result
+        return ResponseEntity.status(HttpStatus.OK).body(dtos);
+    }
+
+    // 2. Create a comment
+    @PostMapping("/api/articles/{articleId}/comments")
+    public ResponseEntity<CommentDto> create(@PathVariable Long articleId, @RequestBody CommentDto dto) {
+        // Request to service
+        CommentDto createdDto = commentService.create(articleId, dto);
+        // Response the result
+        return ResponseEntity.status(HttpStatus.OK).body(createdDto);
+    }
+
+    // 3. Update a comment
+
+    // 4. Delete a comment
 }
--- src/main/java/com/example/firstproject/service/CommentService.java
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -1,4 +1,50 @@
 package com.example.firstproject.service;
 
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service
 public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        /*// 1. Retrieve comments
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+
+        // 2. Entity -> DTO
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for (int i = 0; i < comments.size(); i++) {
+            Comment c = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(c);
+            dtos.add(dto);
+        }*/
+
+        // 3. Return the results
+        return commentRepository.findByArticleId(articleId)
+                .stream()
+                .map(comment -> CommentDto.createCommentDto(comment))
+                .collect(Collectors.toList());//dtos;
+    }
+
+    public CommentDto create(Long articleId, CommentDto dto) {
+        // 1. Read the article & try exception
+
+        // 2. Create entity for the comment
+
+        // 3. Save the comment to DB
+
+        // 4. Return a DTO from
+        return null;
+    }
 }
